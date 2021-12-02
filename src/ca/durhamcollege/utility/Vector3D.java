c192dfcf09f4c67e6b4cb40bdcd33c5252baddca
Katherine Bellman <katherine.bellman@dcmail.ca>
2021-12-01 23:40:23 -0500

    Began Vector3D class

---
+++ src/ca/durhamcollege/utility/Vector3D.java
@@ -0,0 +1,26 @@
+package ca.durhamcollege.utility;
+
+public class Vector3D extends Vector2D
+{
+
+    //PRIVATE INSTANCE VARIABLES
+    private float z;
+
+    //PUBLIC PROPERTIES
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+
+    //CONSTRUCTORS
+
+    //PRIVATE METHODS
+
+    //PUBLIC METHODS
+
+    //STATIC METHODS
+}
